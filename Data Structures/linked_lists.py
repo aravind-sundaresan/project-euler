a459da6c44cdf574efd3bf0a40db482ca56a8d04
whackadoodle <sundaresan.aravind2@gmail.com>
2018-05-16 23:07:08 +0530

    Updated implementation of Linked Lists

--- Data Structures/linked_lists.py
+++ Data Structures/linked_lists.py
@@ -29,6 +29,16 @@ class UnorderedList:
 		temp.setNext(self.head)
 		self.head = temp
 
+	def size(self):
+		count = 0
+		current = self.head
+
+		while current != None:
+			count += 1
+			current = current.getNext()
+
+		return count
+
 if __name__ == '__main__':
 	node = Node(12)
 	print(node.getData())
