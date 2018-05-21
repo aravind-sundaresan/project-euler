dff87de9924e345758bebc8f770c8d9ebf980fd5
whackadoodle <sundaresan.aravind2@gmail.com>
2018-05-21 22:23:29 +0530

    Implementation of Prim's Algorithm

---
+++ Algos/prims_algo.py
@@ -0,0 +1,7 @@
+def prims(n, cost_adjacency_matrix):
+
+
+if __name__ == '__main__':	
+	n = 6
+	cost_adjacency_matrix = [[0, 3, 99, 99, 6, 5], [3, 0, 1, 99, 99, 4], [99, 1, 0, 6, 99, 4], [99, 99, 6, 0, 8, 5], [6, 99, 99, 8, 0, 2], [5, 4, 4, 5, 2, 0]]
+	source_vertex = 1
