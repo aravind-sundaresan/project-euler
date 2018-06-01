77f39d930b1a320a75f70baf5f1e3e9f60aaef89
whackadoodle <sundaresan.aravind2@gmail.com>
2018-06-01 09:56:24 +0530

    Factorial using Recursion implementation

---
+++ Interview_Questions/factorial_recursion.py
@@ -0,0 +1,14 @@
+# Question: Find the Factorial of a number using Recursion
+
+def factorial(number):
+
+	if number <= 1:
+		return number
+
+	return (number * factorial(number - 1))
+
+
+if __name__ == '__main__':
+	
+	result = factorial(5)
+	print(result)
